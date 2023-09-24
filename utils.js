5a64596cb7475109592778ba2ecea89666fbf51e
SolenPB <solene.paratre@gmail.com>
2023-09-24 09:10:50 +0200

    ajout js

---
+++ utils.js
@@ -0,0 +1,9 @@
+let newPage = document.getElementsById("inIcon");
+newPage.addEventListener("click", function(){
+    window.open('', '_blank');
+})
+
+let navButton = document.getElementsByClassName("navButton");
+
+
+

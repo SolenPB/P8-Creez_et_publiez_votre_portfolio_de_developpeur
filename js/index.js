868201a727ad3f97e9d6ea442d19d26f34a39027
SolenPB <solene.paratre@gmail.com>
2023-09-28 16:50:59 +0200

    maj dyna portfolio

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-console.log(0);
+const pictures = document.getElementsByClassName("scan");
+
+for(let i = 0; i < 6; i++){
+    pictures[i].addEventListener("mouseover", function(){
+        pictures[i].classList.add("zoomIn")
+        try{
+            pictures[i].classList.remove("zoomOut")
+        } catch {}
+    })
+    pictures[i].addEventListener("mouseout", function(){
+        if(pictures[i].classList.contains("zoomIn")){
+            pictures[i].classList.add("zoomOut")
+        try{
+            pictures[i].classList.remove("zoomIn")
+        } catch {}
+        }
+    })
+}
